a1fe7f28b3f14c7ccd5aaef1e0328eca5fa537df
Hemal99 <h.githmin@gmail.com>
2023-06-28 14:04:22 +0530

    fix : updated field

--- src/components/PromptPage/index.tsx
+++ src/components/PromptPage/index.tsx
@@ -98,6 +98,8 @@ function PromptPage(params: any) {
               name="inputParams"
               id="inputParams"
               value={formik.values.inputParams}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               placeholder="Enter your inputParams"
               className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
             />
