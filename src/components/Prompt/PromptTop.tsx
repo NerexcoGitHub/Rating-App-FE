42156f10cd5393cea8e09bafa0c70ebe7cd84546
Haritha Hasathcharu <harithahasathcharu@icloud.com>
2023-09-21 18:54:51 +0530

    comments

--- src/components/Prompt/PromptTop.tsx
+++ src/components/Prompt/PromptTop.tsx
@@ -1,34 +1,57 @@
-import React, { useMemo, useState } from "react";
-import Image from "next/image";
-import { motion } from "framer-motion";
-import getScrollAnimation from "../../utils/getScrollAnimation";
-import ButtonPrimary from "../Misc/ButtonPrimary";
-import { Breadcrumbs, IconButton, Rating, Typography } from "@mui/material";
-import Avatar from "../Misc/Avatar";
-import { combineClasses } from "../../utils/utils";
-import LinkTo from "../LinkTo";
-import classes from "../ArticleCards/ArticleCard.module.scss";
-import ArticleTags from "../Misc/ArticleTags";
-import CopyToClipboard from "react-copy-to-clipboard";
-import ContentCopyIcon from "@mui/icons-material/ContentCopy";
-import ShareIcon from "@mui/icons-material/Share";
-import { publicRequest } from "../../../config/axiosRequest";
-import { useCookies } from "react-cookie";
-import { useRouter } from "next/router";
-import StarIcon from "@mui/icons-material/Star";
-import CopyIconComponent from "../CopyIconComponent";
-import ShareIconComponent from "../ShareIconComponent";
+import React, { useMemo, useState } from 'react';
+import Image from 'next/image';
+import getScrollAnimation from '../../utils/getScrollAnimation';
+import {
+  Breadcrumbs,
+  IconButton,
+  Rating,
+  Typography,
+  TextareaAutosize,
+} from '@mui/material';
+import Avatar from '../Misc/Avatar';
+import { combineClasses } from '../../utils/utils';
+import LinkTo from '../LinkTo';
+import classes from '../ArticleCards/ArticleCard.module.scss';
+import ArticleTags from '../Misc/ArticleTags';
+import { publicRequest } from '../../../config/axiosRequest';
+import { useCookies } from 'react-cookie';
+import { useRouter } from 'next/router';
+import StarIcon from '@mui/icons-material/Star';
+import CopyIconComponent from '../CopyIconComponent';
+import ShareIconComponent from '../ShareIconComponent';
 
 const PromptTop = (props: any) => {
   const scrollAnimation = useMemo(() => getScrollAnimation(), []);
-  const [cookies, setCookie, removeCookie] = useCookies(["deviceId"]);
+  const [cookies, setCookie, removeCookie] = useCookies(['deviceId']);
+  const [touched, setTouched] = useState<string[]>([]);
+  const [commentSubmitted, setCommentSubmitted] = useState(false);
+  const [comments, setComments] = useState([
+    'This is a comment',
+    'This is another comment',
+  ]);
+  const [newComment, setNewComment] = useState('');
+  const handleCommentChange = (e) => {
+    setNewComment(e.target.value);
+  };
+
+  const handleSubmitComment = () => {
+    if (commentSubmitted) {
+      return;
+    }
+    if (newComment.trim() !== '') {
+      //make api call
+      setCommentSubmitted(true);
+      setComments([newComment, ...comments]);
+      setNewComment('');
+    }
+  };
   let router = useRouter();
   const [currentRate, setCurrentRate] = useState({
     rating: 0,
-    id: "",
+    id: '',
   });
 
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState('');
 
   const {
     _id,
@@ -51,11 +74,16 @@ const PromptTop = (props: any) => {
   const matches = prompt.match(regex);
 
   const handleCreatePrompt = async () => {
+    setTouched([...touched, 'promptCreated']);
     const modifiedSentence = prompt.replace(
       regex,
       (match: any, word: any) => inputParamSet[word] || match
     );
     setResult(modifiedSentence);
+  };
+  console.log(touched);
+  const getAllTouched = () => {
+    return touched.length >= matches?.length + 1;
   };
   const handleRate = async (value: number | null) => {
     setCurrentRate({
@@ -85,83 +113,63 @@ const PromptTop = (props: any) => {
   };
 
   return (
-    <div className="max-w-screen-xl mt-0 px-8 xl:px-16 mx-auto " id="about">
-      <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start lg:space-x-8 space-y-8 lg:space-y-0 mt-20 w-full">
-        <div className="flex flex-col justify-center items-start row-start-2 sm:row-start-1 w-full lg:w-3/4   border-y-neutral-500 sm:border-none">
-          {checkIfRated(ratingList) ? (
-            <div className="m-4">
-              <p className="text-[11px] text-red-500">
-                You have already rated this prompt
-              </p>
-            </div>
-          ) : (
-            <div className="mt-4">
-              <p className="text-[15px] text-grey-500">Rate this Prompt</p>
-              <Rating
-                size="large"
-                name="size-large"
-                defaultValue={0}
-                value={currentRate.id === _id ? currentRate.rating : 0}
-                onChange={(event, newValue) => {
-                  handleRate(newValue);
-                }}
-                readOnly={currentRate.rating !== 0 && currentRate.id === _id}
-              />
-            </div>
-          )}
-
-          <h1 className="text-xl lg:text-xl xl:text-xl font-medium text-black-600 leading-normal ">
+    <div className='max-w-screen-xl mt-0 px-8 xl:px-16 mx-auto ' id='about'>
+      <div className='flex flex-col lg:flex-row justify-between items-center lg:items-start lg:space-x-8 space-y-8 lg:space-y-0 mt-20 w-full'>
+        <div className='flex flex-col justify-center items-start row-start-2 sm:row-start-1 w-full lg:w-3/4   border-y-neutral-500 sm:border-none'>
+          <h1 className='text-lg lg:text-xl xl:text-xl font-lg text-black-600 leading-normal '>
             Customize for Yourself!
           </h1>
-          <div className="flex flex-col  justify-between items-center lg:items-start lg:space-x-8 space-y-8 lg:space-y-0 w-full">
-            <div className="mt-6 w-full">
+          <div className='flex flex-col  justify-between items-center lg:items-start lg:space-x-8 space-y-8 lg:space-y-0 w-full'>
+            <div className='mt-6 w-full'>
               <label
-                className="block mb-4 text-sm font-bold text-gray-700"
-                htmlFor="authorName"
+                className='block mb-4 text-sm font-bold text-gray-700'
+                htmlFor='authorName'
               >
                 Input Params
               </label>
-              <div className="flex flex-col">
+              <div className='flex flex-col'>
                 {matches?.length > 0 &&
-                  matches.map((match: any) => (
-                    <>
+                  matches.map((match: any, i) => (
+                    <div key={i}>
                       <label
-                        className="block m-1 text-sm font-bold text-gray-700"
-                        htmlFor="authorName"
+                        className='block m-1 text-sm font-bold text-gray-700'
+                        htmlFor='authorName'
                       >
                         {match.slice(1, -1)}
                       </label>
                       <input
-                        className="px-4 w-full sm:w-1/2 mb-2 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                        className='px-4 w-full sm:w-1/2 mb-2 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline'
                         id={match.slice(1, -1)}
-                        type="text"
+                        type='text'
                         name={match.slice(1, -1)}
                         placeholder={match}
-                        onChange={(e) =>
+                        onChange={(e) => {
+                          if (!touched.includes(match.slice(1, -1)))
+                            setTouched([...touched, match.slice(1, -1)]);
                           setInputParamSet(
                             Object.assign({}, inputParamSet, {
                               [match.slice(1, -1)]: e.target.value,
                             })
-                          )
-                        }
+                          );
+                        }}
                       />
-                    </>
+                    </div>
                   ))}
 
                 <button
-                  className="px-4 w-full sm:w-1/2 mt-3 py-2 text-sm leading-tight text-wh border rounded shadow appearance-none focus:outline-none focus:shadow-outline bg-orange-500"
+                  className='px-4 w-full sm:w-1/2 mt-3 py-2 text-sm leading-tight text-wh border rounded shadow appearance-none focus:outline-none focus:shadow-outline bg-orange-500'
                   onClick={handleCreatePrompt}
                 >
                   Create Prompt
                 </button>
               </div>
 
-              <div className="flex-col w-full">
-                <div className=" w-full mt-5  border-2 p-5 bg-slate-300  rounded-lg hover:border-black">
-                  <h1 className="text-xl lg:text-2xl xl:text-2xl font-medium text-black-600 leading-normal">
+              <div className='flex-col w-full'>
+                <div className=' w-full mt-5  border-2 p-5 bg-slate-300  rounded-lg hover:border-black'>
+                  <h1 className='text-xl lg:text-2xl xl:text-2xl font-medium text-black-600 leading-normal'>
                     {result && <strong>{result}</strong>}
                   </h1>
-                  <div className="flex mt-2">
+                  <div className='flex mt-2'>
                     <CopyIconComponent text={result} />
                   </div>
                 </div>
@@ -175,13 +183,13 @@ const PromptTop = (props: any) => {
               </div>
             </div>
 
-            <div className="w-full"></div>
+            <div className='w-full'></div>
           </div>
         </div>
 
-        <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start lg:space-x-8 space-y-8 lg:space-y-0 mt-20 w-full">
-          <div className="flex flex-col justify-center items-start row-start-2 sm:row-start-1  border-y-neutral-500 sm:border-none">
-            <p className={"font-normal text-xs pt-3 mb-0 md:mb-3"}>
+        <div className='flex flex-col lg:flex-row justify-between items-center lg:items-start lg:space-x-8 space-y-8 lg:space-y-0 mt-20 w-full'>
+          <div className='flex flex-col justify-center items-start row-start-2 sm:row-start-1  border-y-neutral-500 sm:border-none'>
+            <p className={'font-normal text-xs pt-3 mb-0 md:mb-3'}>
               Updated on : {createdAt?.slice(0, 10)}
             </p>
 
@@ -189,28 +197,28 @@ const PromptTop = (props: any) => {
 
             <div
               className={
-                "flex items-center justify-between mt-3 flex-col sm:flex-row"
+                'flex items-center justify-between mt-3 flex-col sm:flex-row'
               }
             >
-              <div className={"flex items-center"}>
+              <div className={'flex items-center'}>
                 <Avatar
                   author={author}
-                  className="w-[40px] h-[40px] mr-3 text-xl"
+                  className='w-[40px] h-[40px] mr-3 text-xl'
                 />
                 <LinkTo
-                  href={""}
+                  href={''}
                   passHref
                   className={combineClasses(
                     classes.author_name,
-                    "text-sm font-medium"
+                    'text-sm font-medium'
                   )}
                 >
                   {author?.userName} ({author?.designation})
                 </LinkTo>
               </div>
-              <div className="flex items-start sm:items-center ml-10">
-                <StarIcon className="text-yellow-500" />
-                <span className="text-[15px] leading-6 text-slate-700 mt-1">
+              <div className='flex items-start sm:items-center ml-10'>
+                <StarIcon className='text-yellow-500' />
+                <span className='text-[15px] leading-6 text-slate-700 mt-1'>
                   {rating} ({ratecount})
                 </span>
               </div>
@@ -232,22 +240,22 @@ const PromptTop = (props: any) => {
                 based on {ratecount} user ratings
               </span>
             </div> */}
-            <div className={"flex items-center justify-center mt-2 mb-2"}>
+            <div className={'flex items-center justify-center mt-2 mb-2'}>
               <Breadcrumbs maxItems={2}>
-                <Typography color="textPrimary">{category}</Typography>
-                <Typography color="textPrimary">{subCategories}</Typography>
+                <Typography color='textPrimary'>{category}</Typography>
+                <Typography color='textPrimary'>{subCategories}</Typography>
               </Breadcrumbs>
             </div>
-            <h1 className="text-xl lg:text-2xl xl:text-2xl font-medium text-black-600 leading-normal">
+            <h1 className='text-xl lg:text-2xl xl:text-2xl font-medium text-black-600 leading-normal'>
               {title}
             </h1>
-            <p className="text-black-500 mt-4">{description}</p>
+            <p className='text-black-500 mt-4'>{description}</p>
 
-            <div className="relative w-full mt-5 border-2 p-5 bg-slate-300 rounded-lg hover:border-black">
-              <h1 className="text-xl lg:text-2xl xl:text-2xl font-medium text-black-600 leading-normal ">
+            <div className='relative w-full mt-5 border-2 p-5 bg-slate-300 rounded-lg hover:border-black'>
+              <h1 className='text-xl lg:text-2xl xl:text-2xl font-medium text-black-600 leading-normal '>
                 <strong>{prompt}</strong>.
               </h1>
-              <div className="flex mt-2">
+              <div className='flex mt-2'>
                 <CopyIconComponent text={prompt} />
                 {/* <CopyToClipboard
                   text={`${process.env.NEXT_PUBLIC_PROMPT_URL}${_id}`}
@@ -257,11 +265,64 @@ const PromptTop = (props: any) => {
                   </IconButton>
 
                 </CopyToClipboard> */}
-                <div className="ml-2"></div>
-                <ShareIconComponent text={`${process.env.NEXT_PUBLIC_PROMPT_URL}${_id}`} />
+                <div className='ml-2'></div>
+                <ShareIconComponent
+                  text={`${process.env.NEXT_PUBLIC_PROMPT_URL}${_id}`}
+                />
               </div>
             </div>
           </div>
+        </div>
+      </div>
+      {checkIfRated(ratingList) ? (
+        <div className='m-4 ml-4'>
+          <p className='text-[11px] text-red-500 text-lg'>
+            You have already rated this prompt
+          </p>
+        </div>
+      ) : getAllTouched() ? (
+        <div className='max-w-lg mt-4 ml-4'>
+          <p className='text-[20px] text-grey-500 mb-1'>Rate this Prompt</p>
+          <Rating
+            size='large'
+            name='size-large'
+            defaultValue={0}
+            value={currentRate.id === _id ? currentRate.rating : 0}
+            onChange={(event, newValue) => {
+              handleRate(newValue);
+            }}
+            readOnly={currentRate.rating !== 0 && currentRate.id === _id}
+          />
+          <div className='mt-4'>
+            <TextareaAutosize
+              className='px-4  mb-2 py-2 text-sm leading-tight text-gray-700  shadow appearance-none focus:outline-none focus:shadow-outline w-full border border-gray-300 rounded-md'
+              placeholder='Add a comment...'
+              value={newComment}
+              onChange={handleCommentChange}
+              minRows={3}
+            />
+          </div>
+
+          <div className='mt-2'>
+            <button
+              className='px-4 w-full sm:w-1/2 mt-3 py-2 text-sm leading-tight text-wh border rounded shadow appearance-none focus:outline-none focus:shadow-outline bg-orange-500'
+              onClick={handleSubmitComment}
+            >
+              Post Comment
+            </button>
+          </div>
+        </div>
+      ) : null}
+      <div className='max-w-lg p-4'>
+        <br />
+        <h2 className='text-2xl font-semibold mb-4'>Comments</h2>
+
+        <div className='space-y-4'>
+          {comments.map((comment, index) => (
+            <div key={index} className='bg-white p-4 rounded-lg w-full'>
+              {comment}
+            </div>
+          ))}
         </div>
       </div>
 
